204a4d108abca5d85d25ab4f82a898f544829bb8
kmkwak <delphinus76@gmail.com>
2015-01-13 15:06:34 +0900

    debug.

--- www/httprequest.js
+++ www/httprequest.js
@@ -1,42 +1,39 @@
-cordova.define("cordova.plugin.httprequest.httprequest", function (require, exports, module) {
+var exec = require('cordova/exec');
 
-    var exec = require('cordova/exec');
+function HttpRequest() {
 
-    function HttpRequest() {
+}
 
+HttpRequest.prototype.echo = function (params, callback) {
+    var options = [];
+    options.push(params.url);
+    options.push(params.cookie);
+    var temp = [];
+    var headers = params.headers;
+    for (var prop in headers) {
+        temp.push({Key:prop, Value: headers[prop]});
     }
+    options.push(JSON.stringify(temp));
+    
+    exec(callback, function (err) {
+        callback('Nothing to echo.');
+    }, "HttpRequest", "echo", [JSON.stringify(options)]);
+};
 
-    HttpRequest.prototype.echo = function (params, callback) {
-        var options = [];
-        options.push(params.url);
-        options.push(params.cookie);
-        var temp = [];
-        var headers = params.headers;
-        for (var prop in headers) {
-            temp.push({Key:prop, Value: headers[prop]});
-        }
-        options.push(JSON.stringify(temp));
-        
-        exec(callback, function (err) {
-            callback('Nothing to echo.');
-        }, "HttpRequest", "echo", [JSON.stringify(options)]);
-    };
+HttpRequest.prototype.request = function (params, callback) {
+    var options = [];
+    options.push(params.url);
+    options.push(params.cookie);
+    var temp = [];
+    var headers = params.headers;
+    for (var prop in headers) {
+        temp.push({ Key: prop, Value: headers[prop] });
+    }
+    options.push(JSON.stringify(temp));
 
-    HttpRequest.prototype.request = function (params, callback) {
-        var options = [];
-        options.push(params.url);
-        options.push(params.cookie);
-        var temp = [];
-        var headers = params.headers;
-        for (var prop in headers) {
-            temp.push({ Key: prop, Value: headers[prop] });
-        }
-        options.push(JSON.stringify(temp));
+    exec(callback, function (err) {
+        callback();
+    }, "HttpRequest", "request", [JSON.stringify(options)]);
+};
 
-        exec(callback, function (err) {
-            callback();
-        }, "HttpRequest", "request", [JSON.stringify(options)]);
-    };
-
-    module.exports.httpRequest = new HttpRequest();
-});
+module.exports.httpRequest = new HttpRequest();
